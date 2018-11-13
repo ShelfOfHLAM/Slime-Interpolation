d2df5e0711bd5958950eef4623d3c7cccd6eac63
SWH <almachino@yandex.ru>
2018-11-13 10:21:12 +0500

    Error found

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-double* run(int n, double a, double b, double h ,double* y)
+long double* run(int n, long double a, long double b, long double h ,long double* y)
 {
-	double **arr = new double*[4];
-	double *m = new double[n + 1];
+	long double **arr = new long double*[4];
+	long double *m = new long double[n + 1];
 	
 	for (int i = 0; i < 4; i++)
 	{
-		arr[i] = new double[n + 1];
+		arr[i] = new long double[n + 1];
 
 	}
 
@@ -23,10 +23,10 @@ double* run(int n, double a, double b, double h ,double* y)
 
 	for (int i = 1; i < n; i++)
 	{
-		arr[0][i] = 2 * h / 3;
-		arr[1][i] = h / 6;
-		arr[2][i] = h / 6;
-		arr[3][i] = (y[i + 1] - y[i]) / h - (y[i] - y[i - 1]) / h;
+		arr[0][i] = 2 * (h / 3);
+		arr[1][i] = (h / 6);
+		arr[2][i] = (h / 6);
+		arr[3][i] = ((y[i + 1] - y[i]) / h) - ((y[i] - y[i - 1]) / h);
 
 	}
 
@@ -34,16 +34,16 @@ double* run(int n, double a, double b, double h ,double* y)
 	arr[2][n] = 0;
 	arr[3][n] = 0 ;
 
-	double *mu = new double[n + 1];
-	double *l = new double[n + 1];
+	long double *mu = new long double[n + 1];
+	long double *l = new long double[n + 1];
 	
-	mu[0] = arr[3][0] / arr[0][0];
-	l[0] = -arr[1][0] / arr[0][0];
+	mu[0] = (arr[3][0] / arr[0][0]);
+	l[0] = (-arr[1][0] / arr[0][0]);
  
 	for (int i = 1; i < n+1; i++)
 	{
-		mu[i] = (arr[3][i] - arr[2][i] * mu[i - 1]) / (arr[0][i] + arr[2][i] * l[i - 1]);
-		l[i] = -arr[1][i] / (arr[0][i] + arr[2][i] * l[i - 1]);
+		mu[i] = ((arr[3][i] - arr[2][i] * mu[i - 1]) / (arr[0][i] + arr[2][i] * l[i - 1]));
+		l[i] = (-arr[1][i] / (arr[0][i] + arr[2][i] * l[i - 1]));
 
 	}
 
@@ -67,11 +67,11 @@ double* run(int n, double a, double b, double h ,double* y)
 	return m;
 }
 
-double s3(int n,double* x,double* y ,double* m ,double h,double xp)
+long double s3(int n,long double* x,long double* y ,long double* m ,long double h,long double xp)
 {
 	for (int i = 1; i < n + 1; i++)
 	{
-		if (x[i - 1] <= xp && xp >= x[i])
+		if (x[i - 1] <= xp && xp <= x[i])
 			return (pow(x[i] - xp, 3) - pow(h, 2)*(x[i] - xp))*m[i - 1] / (6 * h) + 
 				(pow(xp - x[i - 1], 3) - pow(h, 2)*(xp - x[i - 1]))*m[i] / (6 * h) + 
 					(x[i] - xp)*y[i - 1] / h + (xp - x[i - 1])*y[i] / h;
@@ -81,15 +81,15 @@ double s3(int n,double* x,double* y ,double* m ,double h,double xp)
 
 int main()
 {
-	double a = 0;
-	double b = PI ;
-	double deltaMaxP;
+	long double a = 0;
+	long double b = PI ;
+	long double deltaMaxP;
 	
 	for (int n = 5; n <= 10240; n *= 2)
 	{
-		double *x = new double[n + 1];
-		double *y = new double[n + 1];
-		double h = (b - a) / n;
+		long double *x = new long double[n + 1];
+		long double *y = new long double[n + 1];
+		long double h = ((b - a) / n);
 
 		x[0] = a;
 		x[n] = b;
@@ -102,12 +102,12 @@ int main()
 
 		}
 
-		double deltaMax = 0;
-		double *m = run(n, a, b, h, y);
+		long double deltaMax = 0;
+		long double *m = run(n, a, b, h, y);
 
 		for (int i = 0; i < n; i++)
 		{
-			double delta = abs(s3(n, x, y, m, h, x[i] + h / 2) - sin(x[i] + h / 2));
+			long double delta = abs(s3(n, x, y, m, h, x[i] + (h / 2)) - sin(x[i] +(h / 2)));
 
 			if (delta > deltaMax)
 				deltaMax = delta;
@@ -120,7 +120,7 @@ int main()
 		}
 		else
 		{
-			cout << n << " " << deltaMax << " " << deltaMaxP / deltaMax <<endl;
+			cout << n << " " << deltaMax << " " << deltaMaxP/pow(2,4) << " " <<(deltaMaxP / deltaMax) <<endl;
 
 		}
 
